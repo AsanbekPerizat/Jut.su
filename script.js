af6f77a976f62161994e01c87d111403f9993aa1
AsanbekPerizat <101558116+AsanbekPerizat@users.noreply.github.com>
2022-05-23 16:07:19 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function navmenu(){
+    document.getElementById("sidebar").style.display="block"
+    document.getElementById("sidebar2").style.display="block"
+}
+
+function zhaby(){
+    document.getElementById("sidebar2").style.display="none"
+    document.getElementById("sidebar").style.display="none"
+}
